a385329f14831ebbb1ea752e3dd6ff99ccf1f7dc
Dev center <lmauge.joan76@gmail.com>
2026-07-10 08:47:41 +0200

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Configuration Supabase — Je Raconte, Tu T'envoles
-const SUPABASE_URL = "https://ezkrzbcilmfgdsiybyzo.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6a3J6YmNpbG1mZ2RzaXlieXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0ODc3NzAsImV4cCI6MjA5OTA2Mzc3MH0.QTPxcFhz48Ag5v_NpBG8znhDBlWI9N29Z3wmS1Cgdo8";
