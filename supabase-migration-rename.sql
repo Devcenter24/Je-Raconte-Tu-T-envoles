bdf417cceeb6f51d9591d4665d4831401f3a32d7
Dev center <lmauge.joan76@gmail.com>
2026-07-10 08:48:48 +0200

    Delete supabase-migration-rename.sql

--- supabase-migration-rename.sql
+++
@@ -1,4 +0,0 @@
--- Si tu as DÉJÀ exécuté l'ancien script (table "comptes" existante),
--- exécute simplement ceci dans Supabase → SQL Editor pour la renommer :
-
-alter table comptes rename to contes;
